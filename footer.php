5a1a7cb1e534a9b3ec5bb896397d026f398fffa0
toqashaat <101115117+toqashaat@users.noreply.github.com>
2022-08-22 13:06:50 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+
+</head>
+
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+<footer>
+This is a Footer
+</footer>
+</body>
+</html>
+?>
